cb505229034c32ce926ae2e140fb77fbce91b468
Codey Whitt <codey.whitt@gmail.com>
2020-04-21 18:39:03 -0600

    init commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/sinisterminister/moneytree
+
+go 1.13
+
+require (
+	github.com/go-playground/ansi v2.1.0+incompatible // indirect
+	github.com/go-playground/log v6.3.0+incompatible
+	github.com/go-playground/log/v7 v7.0.2
+	github.com/preichenberger/go-coinbasepro/v2 v2.0.5
+	github.com/sinisterminister/currencytrader v0.0.0-20200314004648-7529a2dcf9ac
+	github.com/spf13/viper v1.6.2
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+
+	"github.com/go-playground/log/v7"
+	"github.com/go-playground/log/v7/handlers/console"
+	"github.com/preichenberger/go-coinbasepro/v2"
+	"github.com/sinisterminister/currencytrader"
+	"github.com/sinisterminister/currencytrader/types/provider/coinbase"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// Setup the console logger
+	log.AddHandler(console.New(true), log.InfoLevel, log.WarnLevel, log.ErrorLevel, log.NoticeLevel, log.FatalLevel, log.AlertLevel, log.PanicLevel)
+
+	// Setup the kill switch
+	killSwitch := make(chan bool)
+
+	// Setup a coinbase client
+	client := coinbasepro.NewClient()
+
+	// Connect to sandbox
+	client.UpdateConfig(&coinbasepro.ClientConfig{
+		BaseURL:    "https://api-public.sandbox.pro.coinbase.com",
+		Key:        "db983743c2fa020a17502a111657b551",
+		Passphrase: "throwback",
+		Secret:     "SrHvi/n9HAcEoe/JXsaZlfok4O/hXULiK4OhoANFN5GS0odp5ciho1w1jmMXlQ40Br8G8GU6WGRPClmbQnUyEQ==",
+	})
+
+	// Setup sandbox websocket url
+	viper.Set("coinbase.websocket.url", "wss://ws-feed-public.sandbox.pro.coinbase.com")
+
+	// Start up a coinbase provider
+	provider := coinbase.New(killSwitch, client)
+
+	// Get an instance of the trader
+	trader := currencytrader.New(provider)
+	trader.Start()
+
+	// Get the available markets
+	btc, err := trader.WalletSvc().Currency("BTC")
+	if err != nil {
+		log.WithError(err).Fatal("could not get BTC")
+	}
+	usdc, err := trader.WalletSvc().Currency("USDC")
+	if err != nil {
+		log.WithError(err).Fatal("could not get USDC")
+	}
+	markets, err := trader.MarketSvc().Market(btc, usdc)
+	if err != nil {
+		log.WithError(err).Fatal("could not get market BTC-USDC")
+	}
+
+	// Stream the tickers to output log
+	for _, mkt := range markets {
+		if mkt.Name() == "BTC-USD" {
+			go placeOrders(killSwitch, mkt)
+		}
+	}
+
+	// Intercept the interrupt signal and pass it along
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	// Wait for the interrupt
+	<-interrupt
+
+	// Let the user know what happened
+	log.Warn("Received an interrupt signal! Shutting down!")
+
+	// Shutdown the trader
+	trader.Stop()
+
+	// Kill the provider
+	close(killSwitch)
+}
