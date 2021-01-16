d61465a22830b91ec896e0e94f8373fee6b1911d
Codey <codey.whitt@gmail.com>
2021-01-16 00:10:55 -0700

    clean up old code

--- lib/orderpair/init.go
+++
@@ -1,8 +0,0 @@
-package orderpair
-
-import "github.com/spf13/viper"
-
-func init() {
-	viper.SetDefault("orderpair.missPercentage", 0.05)
-	viper.SetDefault("orderpair.criticalPairRefreshInterval", "1m")
-}
