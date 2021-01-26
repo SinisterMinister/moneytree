97a0cd1777f617b884d03f3a302aed957ae198ec
Codey <codey.whitt@gmail.com>
2021-01-25 17:45:52 -0700

    use trix indicators to chose the direction when open orders are equal

--- pkg/miraclegrow/svc.go
+++ pkg/miraclegrow/svc.go
@@ -3,12 +3,15 @@ package miraclegrow
 import (
 	"context"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/go-playground/log/v7"
 	"github.com/heptiolabs/healthcheck"
+	"github.com/shopspring/decimal"
 	"github.com/sinisterminister/moneytree/pkg/pair"
 	"github.com/sinisterminister/moneytree/pkg/proto"
+	"github.com/sinisterminister/moneytree/pkg/trix"
 	"google.golang.org/grpc"
 )
 
@@ -63,7 +66,8 @@ func (svc *Service) startWatering() (err error) {
 	defer cancel()
 	pairs, err := svc.moneytree.GetOpenPairs(ctx, &proto.NullRequest{})
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+		log.WithError(err).Error("could not get open pairs")
+		return
 	}
 
 	// Get the the direction with the least number of pairs
@@ -86,9 +90,29 @@ func (svc *Service) startWatering() (err error) {
 	} else if upCount < downCount {
 		svc.placePair(pair.Upward)
 	} else {
-		// We need to place both
-		svc.placePair(pair.Upward)
-		svc.placePair(pair.Downward)
+		// We'll place the pair based on the trix indicators
+		currentPrice, movingAverage, oscillator, err := svc.getCurrentTrixIndicators(ctx)
+		if err != nil {
+			log.WithError(err).Error("could not get trix indicators")
+		}
+
+		// If the current price is above the moving average, going up
+		if currentPrice.GreaterThan(movingAverage) {
+			// Make sure gaining momentum
+			if oscillator.GreaterThanOrEqual(decimal.Zero) {
+				// Place the upward pair
+				svc.placePair(pair.Upward)
+			}
+		}
+
+		// If the current price is below the moving average, going up
+		if currentPrice.LessThan(movingAverage) {
+			// Make sure gaining momentum
+			if oscillator.LessThanOrEqual(decimal.Zero) {
+				// Place the upward pair
+				svc.placePair(pair.Downward)
+			}
+		}
 	}
 
 	return
@@ -117,4 +141,36 @@ func (svc *Service) startHealthcheckHandler() {
 
 	// Expose the /live and /ready endpoints over HTTP (on port 8086)
 	go http.ListenAndServe("0.0.0.0:8086", health)
+}
+
+func (svc *Service) getCurrentTrixIndicators(ctx context.Context) (currentPrice decimal.Decimal, movingAvg decimal.Decimal, oscillator decimal.Decimal, err error) {
+	log.Infof("calculate trix moving average and oscillator")
+	candles, err := svc.moneytree.GetCandles(ctx, &proto.GetCandlesRequest{Duration: proto.GetCandlesRequest_ONE_MINUTE, StartTime: time.Now().Add(-1 * time.Hour).Unix(), EndTime: time.Now().Unix()})
+	if err != nil {
+		log.WithError(err).Error("could not get candles")
+		return
+	}
+
+	// Set the current price
+	currentPrice, err = decimal.NewFromString(candles.GetCandles()[0].Close)
+	if err != nil {
+		log.WithError(err).Error("could not parse current price")
+		return
+	}
+
+	// Convert the candle values to floats so we can use them while reversing the sort
+	rawValues := []float64{}
+	for _, can := range candles.GetCandles() {
+		val, e := strconv.ParseFloat(can.Close, 64)
+		if e != nil {
+			return
+		}
+		rawValues = append([]float64{val}, rawValues...)
+	}
+
+	ma, osc := trix.GetTrixIndicator(rawValues)
+	movingAvg = decimal.NewFromFloat(ma)
+	oscillator = decimal.NewFromFloat(osc)
+	log.Infof("cp: %s ma: %s osc: %s", currentPrice, movingAvg, oscillator)
+	return
 }
